eec914d92e20ab15c083792d4aea78c6240fc6b0
sgrontflix <75019076+sgrontflix@users.noreply.github.com>
2020-11-25 20:40:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sys
 import os
 import re
+import argparse
 from pytube import YouTube
 from moviepy import editor as mpe
 
@@ -15,40 +17,35 @@ def youtube_url_validation(url):
     return youtube_regex_match
 
 
-def combine(video_file, audio_file, out_file, fps=25):
+def combine_tracks(video_file, audio_file, out_file, fps=25):
     video = mpe.VideoFileClip(video_file)
     audio = mpe.AudioFileClip(audio_file)
     out = video.set_audio(audio)
     out.write_videofile(out_file, fps=fps)
 
-    # close reader to avoid PermissionError: [WinError 32]
+    # close readers to avoid PermissionError: [WinError 32] when deleting temporary files
     video.close()
     audio.close()
     out.close()
 
 
-while True:
-    yt_url = input("\nEnter the URL: ")
-    yt_url = yt_url.strip()
+parser = argparse.ArgumentParser()
+parser.add_argument("url", help="URL of the video you want to download")
+parser.add_argument("-a", action="store_true", help="Only download the audio")
 
-    if not yt_url or not youtube_url_validation(yt_url):
-        print("Please, provide a valid YouTube URL.")
-    else:
-        break
+args = parser.parse_args()
 
-while True:
-    audio_only = input("\nDo you want to only download the audio? (y/n): ")
-    audio_only = audio_only.strip().upper()
+yt_url = args.url.strip()
+audio_only = args.a
 
-    if audio_only == "Y" or audio_only == "N":
-        break
-    else:
-        print("Please, only enter 'y' or 'n'.")
+if not youtube_url_validation(yt_url):
+    print("Please, provide a valid YouTube URL.")
+    sys.exit()
 
 yt = YouTube(yt_url)
 audio_track = yt.streams.filter(only_audio=True, file_extension='mp4').order_by('bitrate')[-1]
 
-if audio_only == "Y":
+if audio_only:
     audio_track.download()
 else:
     video_track = yt.streams.filter(progressive=False, file_extension='mp4').order_by('resolution')[-1]
@@ -57,7 +54,7 @@ else:
     video_track.download(filename="video")
     audio_track.download(filename="audio")
 
-    combine("video.mp4", "audio.mp4", title + ".mp4")
+    combine_tracks("video.mp4", "audio.mp4", title + ".mp4")
 
     os.remove("video.mp4")
     os.remove("audio.mp4")
